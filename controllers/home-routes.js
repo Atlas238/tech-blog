0387e66fd5a950f8c0b9ea7701de79c5cb05731a
Atlas <jack@bentsen.us>
2021-08-01 21:52:31 -0700

    first routes, have sequelize problems rn

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,27 +1,31 @@
 const router = require('express').Router();
-const { User, Blogpost } = require('../models');
+const { User, Blogpost, Comment } = require('../models');
 
 // GET all blogposts for homepage
 router.get('/', async (req, res) => {
     try {
-        // const blogData = await Blogpost.findAll({
-        //     include: [
-        //         {
-        //             model: User,
-        //             attributes: ['user_name'],
-        //         },
-        //     ],
-        // });
-        
-        // const blogposts = blogData.map((blogs) => {
-        //     blogs.get({ plain: true })
-        // });
-        // blogposts in curly
-        res.render('homepage', { blogposts })
-
+        const blogsData = await Blogpost.findAll();
+        const blogs = Object.values(JSON.parse(JSON.stringify(blogsData)));
+        console.log(blogs);
+        res.status(200).render('homepage', { blogs });
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 });
+
+// GET single blogpost with comments
+router.get('/blogposts/:id', async (req, res) => {
+    try {
+        const blogData = await Blogpost.findOne({
+            where: {
+                id: req.params.id,
+            },
+        });
+        console.log(blogData);
+        res.status(200).render('singlepost', { blogData })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
